2a3cd85ed80e2bdb09bcb1144e8959d70eef5388
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-06 19:59:02 -0500

    trying to add methods and useEffects

--- src/components/stores/index.js
+++ src/components/stores/index.js
@@ -2,7 +2,34 @@ import React, { useEffect, useState } from 'react';
 import useFetch from '../../hooks/fetch.js';
 
 export default function Stores(){
-    const BBurl = 'https://baristabuddyapi.azurewebsites.net/api'
+    const BBurl = 'https://baristabuddyapi.azurewebsites.net/api/stores'
     const {storeList, setStoreList} = useState([]);
     const { request, response, error, isLoading } = useFetch();
+
+
+
+    const getStores = React.useCallback(()=>{
+        const request = {
+            url: BBurl,
+            options: {method: "get"}
+        }
+        request(request);
+    }, [request]);
+
+    //useEffect for inital Load, Can add more dependencies as needed.
+    useEffect(()=>{
+        getStores();
+    }, [getStores]);
+
+    //setting storelist
+    useEffect(()=>{
+        if(response.length > 0){
+         setStoreList(response);
+        } else {
+            getStores();
+        }
+    }, [response, getStores, setStoreList])
+
+
+
 }
