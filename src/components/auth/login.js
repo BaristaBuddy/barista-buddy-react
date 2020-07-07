d624638f128902d433e01e5bbf7b9c3b172c864f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-06 19:34:49 -0500

    added login component logic

---
+++ src/components/auth/login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import useAuth from './../../contexts/auth';
+
+const Login = () => {
+
+    const auth = useAuth();
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const { username, password } = e.target.elements;
+
+        auth.login(username.value, password.value);
+    }
+
+    const logoutSubmit = e => {
+        e.preventDefault();
+        auth.logout();
+    }
+
+    const user = auth.user;
+    console.log(auth);
+
+    if (user) {
+        return (
+            <div className="login">
+                <h3>Welcome back, {user.username}!</h3>
+                <form onSubmit={logoutSubmit}>
+                    <button>Log Out</button>
+                </form>
+            </div>)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="login">
+            <input placeholder="Username" name="username" />
+            <input placeholder="Password" type="password" name="password" />
+            <button>Login</button>
+        </form>
+    )
+}
+
+export default Login;
