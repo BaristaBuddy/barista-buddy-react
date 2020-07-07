b49aa37f9b061310467df178496ed907951ad815
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-06 22:14:57 -0500

    built out footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+import '../../index.scss';
+
+export default function Footer() {
+    
+    return (
+        <footer>
+            <div className="theme-buttons">
+                <Button variant="Secondary">
+                    Color: Original
+                </Button>
+                <Button variant="Secondary">
+                    Color: Dark
+                </Button>
+                <Button variant="Secondary">
+                    Color: Color Blind
+                </Button>
+                </div>
+            <span>&copy; 2020 Barista Buddy</span>
+            <Link path="/about">
+            About the Developers
+            </Link>
+        </footer>
+
+    )
+}
