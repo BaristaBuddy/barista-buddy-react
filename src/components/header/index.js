4a27cc1812635cd796294278657cb6e6538007e9
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-06 20:10:26 -0500

    commented out erroring login. Added head image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Login from "../auth/login";
 
+import BBbrown from '../../assets/barista-buddy-brown.png';
+
 export default function Header() {
     //const theme = useTheme();
 
     return (
         <header>
+            <img alt="Barista Buddy Logo in soft caramel" src={BBbrown} />
             <div className="users">
-            <Login />
+                {/* <Login /> */}
             </div>
-            </header>
+        </header>
     )
 }
