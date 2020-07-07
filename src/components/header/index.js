29dbe7f5d9137ccb5349e98899c29ff2b3debf6c
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-07 08:08:40 -0500

    added div for styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Login from "../auth/login";
 
 import '../../index.scss';
-import BBbrown from '../../assets/barista-buddy-brown.png';
+import bbBrownMobile from '../../assets/bb-brown-mobile.png';
 
 export default function Header() {
     //const theme = useTheme();
 
     return (
         <header>
-            {/* <div className="head-image"> */}
-            <img alt="Barista Buddy Logo in soft caramel" src={BBbrown} />
-            {/* </div> */}
+            <div className="image-wrapper">
+            <img alt="Barista Buddy Logo in soft caramel" src={bbBrownMobile} />
+            </div>
             <div className="users">
                 {/* <Login /> */}
             </div>
