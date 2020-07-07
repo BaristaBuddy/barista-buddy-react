3a87b6d341d3fe48e717b01f46e36b5b4348ac66
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-06 21:21:27 -0500

    trying to return list, currently stuck due to api cors policy

--- src/components/stores/index.js
+++ src/components/stores/index.js
@@ -1,37 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import useFetch from '../../hooks/fetch.js';
 
-export default function Stores(){
+export default function Stores() {
+    //needs to be dontenv
+    //const todoAPI = 'https://deltav-todo.azurewebsites.net/api/v1/todos';
     const BBurl = 'https://baristabuddyapi.azurewebsites.net/api/stores'
-    const {storeList, setStoreList} = useState([]);
+    const { storeList, setStoreList } = useState([]);
     const { request, response, error, isLoading } = useFetch();
 
 
 
-    const getStores = React.useCallback(()=>{
+    const getStores = React.useCallback(() => {
         const requestbody = {
             url: BBurl,
-            options: {method: "get"}
+            options: { method: "get" }
         }
         request(requestbody);
     }, [request]);
 
     //useEffect for inital Load, Can add more dependencies as needed.
-    useEffect(()=>{
+    useEffect(() => {
         getStores();
     }, [getStores]);
 
     //setting storelist
-    useEffect(()=>{
-        if(response.length > 0){
-         setStoreList(response);
+    useEffect(() => {
+        if (response != null) {
+            //setStoreList(response);
+            console.log(response);
         } else {
             getStores();
         }
     }, [response, getStores, setStoreList]);
 
-return (
-    <h2>Our Partner Stores</h2>
-);
+    return (
+        <>
+        <h2>Our Partner Stores</h2>
+        <ul>
+        {/* {response.map((store) =>(
+            <li><h3>{store.name}</h3>
+            <img src={store.storeImageUrl} alt="store"/>
+            </li>
+        ))} */}
+        </ul>
+        </>
+    );
 
 }
