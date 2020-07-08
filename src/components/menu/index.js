4aab51f28ea954fad16137fc217c77b736f93c02
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-07 20:06:57 -0500

    menu is now getting data from api

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -2,15 +2,42 @@ import React, { useEffect, useState } from 'react';
 import useFetch from '../../hooks/fetch.js';
 import { useParams } from 'react-router-dom';
 
-export default function(props){
+export default function (props) {
+
+    const { storeId } = useParams();
+    const { request, response, error, isLoading } = useFetch();
+    const [menu, setMenu] = useState([]);
+    const BBurl = `https://baristabuddyapi.azurewebsites.net/api/stores/${storeId}/Items`;
 
 
+    const getItems = React.useCallback(() => {
+        const requestbody = {
+            url: BBurl,
+            options: { method: "get" }
+        }
+        request(requestbody);
+    }, [request, BBurl]);
 
-    const {storeId} = useParams();
-    return(
+    //useEffect for inital Load, Can add more dependencies as needed.
+    useEffect(() => {
+        getItems();
+    }, [getItems]);
+
+      //setting storelist
+      useEffect(() => {
+        if (response.length > 0) {
+            console.log(response);
+            setMenu(response);
+            
+        } else {
+            getItems();
+        }
+    }, [response, getItems, setMenu]);
+
+    return (
         <>
-        <h1>This is where the menu goes!</h1>
-        <h3>{storeId}</h3>
+            <h1>This is where the menu goes!</h1>
+            <h3>{storeId}</h3>
         </>
     )
 }
