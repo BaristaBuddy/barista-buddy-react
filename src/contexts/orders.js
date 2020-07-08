1e1f6759245c8ccbe81fbf5851441203ac885624
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-08 11:53:33 -0500

    wrote more complex OrderContext

--- src/contexts/orders.js
+++ src/contexts/orders.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { prettyDOM } from '@testing-library/react';
 
 const ordersAPI = 'https://baristabuddyapi.azurewebsites.net/api/orders/';
 
@@ -13,12 +14,58 @@ export class OrdersProvider extends React.Component {
     super(props);
 
     this.state = {
-      order: {},
+      cart: [],
       currentStore: null,
+      cartCount: 0,
+
+      addItem: this.addItem,
+      removeItem: this.removePaid,
     };
   }
 
+  addNew(item) {
+    let newList = this.props.cart;
 
+    const newItem = {
+      count: 1,
+      id: item.id,
+      name: item.name,
+    }
+
+    const filtered = newList.filter(i => {
+      return i.id === item.id;
+    });
+
+    if (filtered.length > 0) {
+      const pos = newList.map(i => { return i.id; }).indexOf(item.id);
+      newList[pos].count += 1;
+    } else {
+      newList.push(newItem);
+    }
+
+    this.setState({ ...this.state, cart: newList, cartCount: this.getCartCount() });
+  }
+
+  removeItem(index) {
+    const cartList = this.state.cart;
+
+    cartList.splice(index, 1);
+
+    this.setState({ ...this.state, cart: cartList, cartCount: this.getCartCount() });
+  }
+
+  getCartCount() {
+    let count = 0;
+
+    if (this.state.cart.length > 0) {
+
+      this.state.cart.forEach(item => {
+        count += item.count;
+      });
+    }
+
+    return count;
+  }
 
   render() {
     return (
