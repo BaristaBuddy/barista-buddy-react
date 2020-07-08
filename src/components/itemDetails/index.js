550ef2f7537a7d329ea26b3023a202260cff4781
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-08 18:17:26 -0500

    built itemDetails page

---
+++ src/components/itemDetails/index.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, Route } from 'react-router-dom';
+import useFetch from '../../hooks/fetch';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+export default function ItemDetails(props) {
+  console.log(props);
+
+  const { storeId, itemId } = useParams();
+  const { request, response, error, isLoading } = useFetch();
+  const [itemDetails, setItemDetails] = useState([]);
+  const [show, setShow] = useState(false)
+  const BBurl = `https://baristabuddyapi.azurewebsites.net/api/stores/${storeId}/Items/${itemId}`;
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const getItem = React.useCallback(() => {
+    const requestbody = {
+      url: BBurl,
+      options: { method: "get" }
+    }
+    request(requestbody);
+  }, [request, BBurl]);
+
+  //useEffect for initial Load, Can add more dependencies as needed.
+  useEffect(() => {
+    getItem();
+   
+  }, []);
+
+  useEffect(() => {
+    setItemDetails(response);
+  }, [response]);
+  // useEffect(() => {
+  //   if (response.length > 0) {
+  //     console.log(response);
+     
+
+  //   } else {
+  //     getItem();
+  //   }
+  // }, [response, getItem, setItemDetails]);
+
+  //https://flaviocopes.com/how-to-format-number-as-currency-javascript/
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
+  return (
+    <Modal.Dialog
+    {...props}
+    show={show}
+    onHide={handleClose}
+    size="lg"
+    aria-labelledby={itemDetails.name}
+    centered
+    >
+      <form >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Item Details
+        </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {/* <img src={itemDetails.imageUrl} alt={itemDetails.name} /> */}
+          <h2>{itemDetails.name}</h2>
+          <p>{itemDetails.ingredients}</p>
+          {/* {itemDetails.itemModifiers.map((itemModifier) => (
+            <label>
+              {itemModifier.modifierName}
+              <input type="checkbox" name="Modifier" value={itemModifier.modifierName} />
+              {itemModifier.additionalCost}
+            </label>
+          ))} */}
+          {console.log(itemDetails.itemModifiers)}
+          <p>{formatter.format(itemDetails.price)}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            variant="secondary">Close
+          </Button>
+          <Button variant="primary">
+            Add to Cart
+          </Button>
+        </Modal.Footer>
+      </form>
+    </Modal.Dialog>
+  )
+}
