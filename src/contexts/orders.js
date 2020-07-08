8bcbfcf2626361e387b08a3bb57bb8fd585df607
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-08 11:17:18 -0500

    wrote minimum order context logic

--- src/contexts/orders.js
+++ src/contexts/orders.js
@@ -1,2 +1,30 @@
 import React, { useContext } from 'react';
 
+const ordersAPI = 'https://baristabuddyapi.azurewebsites.net/api/orders/';
+
+export const OrdersContext = React.createContext();
+
+export default function useOrders() {
+  return useContext(OrdersContext);
+}
+
+export class OrdersProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: {},
+      currentStore: null,
+    };
+  }
+
+
+
+  render() {
+    return (
+      <OrdersContext.Provider value={this.state}>
+        {this.props.children};
+      </OrdersContext.Provider>
+    )
+  }
+}
