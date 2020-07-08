884d8cbaf301c82cedaf229541b3dc48c3328014
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-07 20:19:41 -0500

    items now being displayed, big pictures again

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import useFetch from '../../hooks/fetch.js';
 import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function (props) {
 
@@ -8,6 +10,7 @@ export default function (props) {
     const { request, response, error, isLoading } = useFetch();
     const [menu, setMenu] = useState([]);
     const BBurl = `https://baristabuddyapi.azurewebsites.net/api/stores/${storeId}/Items`;
+
 
 
     const getItems = React.useCallback(() => {
@@ -23,21 +26,40 @@ export default function (props) {
         getItems();
     }, [getItems]);
 
-      //setting storelist
-      useEffect(() => {
+    //setting storelist
+    useEffect(() => {
         if (response.length > 0) {
             console.log(response);
             setMenu(response);
-            
+
         } else {
             getItems();
         }
     }, [response, getItems, setMenu]);
 
+    //https://flaviocopes.com/how-to-format-number-as-currency-javascript/
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+      })
+
     return (
         <>
-            <h1>This is where the menu goes!</h1>
-            <h3>{storeId}</h3>
+            {menu ? menu.map(item => (
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={item.imageUrl} />
+                    <Card.Body>
+                        <Card.Title>{item.name}</Card.Title>
+                        <Card.Text>
+                            <p>Ingredients: {item.ingredients}</p>
+                            <h3>Price: {formatter.format(item.price)}</h3>
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            )) : <h3>Loading!</h3>}
+
         </>
     )
 }
