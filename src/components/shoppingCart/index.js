8be8fc2a270de0ac81563b11887a497cbdc9e3a8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-07-10 08:40:20 -0500

    styled shopping cart

--- src/components/shoppingCart/index.js
+++ src/components/shoppingCart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useOrders from '../../contexts/orders';
 import { useHistory } from "react-router-dom";
+import './shoppingCart.scss';
 
 function ShoppingCart() {
 
@@ -15,13 +16,16 @@ function ShoppingCart() {
   const cartlist = cart.map((i, index) => {
     return (
       <>
-        <h2>Your Cart</h2>
         <tr key={index}>
-          <td>{i.id}</td>
+          {/* <td>{i.id}</td> */}
           <td>`{i.name}  `</td>
           <td>{'x' + i.count}</td>
           <td>{formatter.format(i.price * i.count)}</td>
-          <td>{<Removebutton item={index} text="Remove Item" func={removeItem} />} </td>
+          <td>
+            <div className="btn btn-primary">
+              {<Removebutton item={index} text="Remove Item" func={removeItem} />}
+            </div>
+          </td>
         </tr>
       </>
     )
@@ -32,27 +36,40 @@ function ShoppingCart() {
       <div style={
         { padding: '15px' }
       }>
+        <h2>Your Cart</h2>
+
         <table className='c'>
           <tr className='thead'>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>QUANTITY</th>
-            <th>ACTIONS</th>
+            {/* <th>ID</th> */}
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
           </tr>
           {cartlist}
         </table>
-        <Removebutton text="Delete Order" func={Reset} />
-        <h3>Total Price: {formatter.format(GetTotalPrice())}</h3>
-        <Removebutton text="Go Back" func={history.goBack} />
-        <Removebutton text="Checkout" func={Reset} />
+        <div className="btn btn-primary">
+          <Removebutton text="Delete Order" func={Reset} />
+        </div>
+        <div className="checkout-wrapper">
+          <h3>Total Price: {formatter.format(GetTotalPrice())}</h3>
+          <div className="btn btn-primary">
+            <Removebutton text="Go Back" func={history.goBack} />
+          </div>
+          <div className="btn btn-primary">
+            <Removebutton text="Checkout" func={Reset} />
+          </div>
+        </div>
       </div>
     )
 
   } else {
     return (
       <>
-        <p className='c'>cart is empty</p>
-        <Removebutton text="Go Back" func={history.goBack} />
+        <h2>Your Cart</h2>
+        <p className='c p'>cart is empty</p>
+        <div className="btn btn-primary">
+          <Removebutton className="btn btn-primary" text="Go Back" func={history.goBack} />
+        </div>
       </>
     )
   }
